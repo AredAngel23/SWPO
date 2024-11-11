872a68dde1ab2ce10694e1a1fe438a80dd42e956
Ared <ared230000@gmail.com>
2024-11-11 11:50:26 -0600

    CRUD User

--- app/app.py
+++ app/app.py
@@ -5,6 +5,7 @@ from views.home_views import home_views
 from views.user_views import user_views
 from views.loan_views import loan_views
 from views.error_views import error_views
+from views.admin_views import admin_views
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my secret key'
@@ -13,7 +14,7 @@ app.register_blueprint(home_views)
 app.register_blueprint(user_views)
 app.register_blueprint(loan_views)
 app.register_blueprint(error_views)
+app.register_blueprint(admin_views)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
